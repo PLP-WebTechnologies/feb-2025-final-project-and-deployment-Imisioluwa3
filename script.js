785790d8f8db45f84f61ae4ab8de503c8c871a6f
Enoch <54731341+Imisioluwa3@users.noreply.github.com>
2025-05-16 12:48:18 +0100

    Preloader setup

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const preloader = document.querySelector('.preloader');
+    const minTime = 3000;
+    const start = Date.now();
+
+    if (preloader) {
+        window.addEventListener('load', function() {
+            const elapsed = Date.now() - start;
+            const remaining = minTime - elapsed > 0 ? minTime - elapsed : 0;
+            setTimeout(() => {
+                preloader.classList.add('hide');
+                setTimeout(() => preloader.remove(), 500);
+            }, remaining);
+        });
+    }
+});
+
+
+// document.addEventListener('scroll', function() {
+//     const scrollPosition = window.scrollY;
+//     const sections = document.querySelectorAll('section');
+//     const navLinks = document.querySelectorAll('.navbar-menu a');
+
+//     sections.forEach((section, index) => {
+//         if (scrollPosition >= section.offsetTop - 50 && scrollPosition < section.offsetTop + section.offsetHeight) {
+//             navLinks.forEach(link => link.classList.remove('active'));
+//             navLinks[index].classList.add('active');
+//         }
+//     });
+// })
+
 const yearEl = document.querySelector('.year');
 const year = new Date().getFullYear();
 yearEl.textContent = year;
